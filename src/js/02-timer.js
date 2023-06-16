69fea818b47d1dfcda8c55790976fda4c485e9ed
misko_16 <119707421+misko16@users.noreply.github.com>
2023-06-15 18:48:08 -0700

    sdfsdf

---
+++ src/js/02-timer.js
@@ -0,0 +1,60 @@
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+import Notiflix from "notiflix";
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    const selectedDate = selectedDates[0];
+
+    if (selectedDate <= new Date()) {
+      Notiflix.Notify.failure("Please choose a date in the future");
+      return;
+    }
+
+    const startButton = document.querySelector('[data-start]');
+    startButton.disabled = false;
+
+    startButton.addEventListener('click', () => {
+      const countdownTimer = setInterval(updateTimer, 1000);
+
+      function updateTimer() {
+        const { days, hours, minutes, seconds } = convertMs(selectedDate - new Date());
+
+        document.querySelector('[data-days]').textContent = addLeadingZero(days);
+        document.querySelector('[data-hours]').textContent = addLeadingZero(hours);
+        document.querySelector('[data-minutes]').textContent = addLeadingZero(minutes);
+        document.querySelector('[data-seconds]').textContent = addLeadingZero(seconds);
+
+        if (selectedDate <= new Date()) {
+          clearInterval(countdownTimer);
+          startButton.disabled = true;
+          Notiflix.Notify.success("Countdown Timer has ended");
+        }
+      }
+    });
+  },
+};
+
+flatpickr("#datetime-picker", options);
+
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+function addLeadingZero(value) {
+  return value.toString().padStart(2, '0');
+}
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,47 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  const delay = getInputValue('delay');
+  const step = getInputValue('step');
+  const amount = getInputValue('amount');
+
+  createPromises(delay, step, amount);
+}
+
+function getInputValue(name) {
+  return parseInt(form.elements[name].value);
+}
+
+function createPromises(delay, step, amount) {
+  let currentDelay = delay;
+
+  for (let position = 1; position <= amount; position++) {
+    createPromise(position, currentDelay)
+      .then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+
+    currentDelay += step;
   }
 }
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
